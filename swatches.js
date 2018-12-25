2e8b96424a84457119a0004f1b195445526aa216
AbusufiyanJ <abujafarullah1996@gmail.com>
2018-12-26 00:21:13 +0530

    added  js file

---
+++ swatches.js
@@ -0,0 +1,8 @@
+var swatches = document.querySelectorAll('.swatches span');
+var root = document.querySelector(':root');
+
+swatches.forEach((swatches)) =>{
+    swatch.addEventListener('click',(e)=>{
+        root.style.setProperty('--theme-color',e.target.style.background);
+    })
+}
