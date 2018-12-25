430a91de518ee60ceaf08841f14ed0387115b0d5
AbusufiyanJ <abujafarullah1996@gmail.com>
2018-12-26 00:27:19 +0530

    working on js files

--- swatches.js
+++ swatches.js
@@ -1,8 +1,8 @@
 var swatches = document.querySelectorAll('.swatches span');
 var root = document.querySelector(':root');
 
-swatches.forEach((swatches)) =>{
-    swatch.addEventListener('click',(e)=>{
+swatches.forEach((swatches)) => {
+    swatch.addEventListener('click',(e) => {
         root.style.setProperty('--theme-color',e.target.style.background);
     })
 }
